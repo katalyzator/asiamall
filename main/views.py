6e36328ad6c94b6228f7f69bf07a14666664f1bb
katalyzator <web.coder96@gmail.com>
2018-02-09 13:53:06 +0600

    change json structure

--- main/views.py
+++ main/views.py
@@ -17,39 +17,38 @@ def get_main_page_values(request):
     try:
         news = News.objects.all()[:3]
         promotions = Promotion.objects.all()[:3]
-        slider = Slider.objects.all()[:3]
+        slider = Slider.objects.all()[:5]
 
         return JsonResponse({
-            "result": {
-                "news": [{
-                    "title": u"{}".format(new.title).encode("utf-8"),
-                    "image": (u"{}".format(new.image.url)).encode("utf-8") if new.image else None,
-                    "video": (u"{}".format(new.video.url)).encode("utf-8") if new.video else None,
-                    "tag": u"{}".format(new.tag).encode("utf-8"),
-                    "text": u"{}".format(new.text).encode("utf-8"),
-                    "timestamp": u"{}".format(new.timestamp).encode("utf-8")
 
-                } for new in news],
+            "news": [{
+                "title": u"{}".format(new.title).encode("utf-8"),
+                "image": (u"{}".format(new.image.url)).encode("utf-8") if new.image else None,
+                "video": (u"{}".format(new.video.url)).encode("utf-8") if new.video else None,
+                "tag": u"{}".format(new.tag).encode("utf-8"),
+                "text": u"{}".format(new.text).encode("utf-8"),
+                "timestamp": u"{}".format(new.timestamp).encode("utf-8")
 
-                "promotions": [{
-                    "title": u"{}".format(promotion.title).encode("utf-8"),
-                    "image": u"{}".format(promotion.image.url).encode("utf-8"),
-                    "tag": u"{}".format(promotion.tag).encode("utf-8"),
-                    "phone_number": u"{}".format(promotion.phone_number).encode("utf-8"),
-                    "text": u"{}".format(promotion.text).encode("utf-8"),
-                    "timestamp": u"{}".format(promotion.timestamp).encode("utf-8")
+            } for new in news],
 
-                } for promotion in promotions],
+            "promotions": [{
+                "title": u"{}".format(promotion.title).encode("utf-8"),
+                "image": u"{}".format(promotion.image.url).encode("utf-8"),
+                "tag": u"{}".format(promotion.tag).encode("utf-8"),
+                "phone_number": u"{}".format(promotion.phone_number).encode("utf-8"),
+                "text": u"{}".format(promotion.text).encode("utf-8"),
+                "timestamp": u"{}".format(promotion.timestamp).encode("utf-8")
 
-                "slider": [{
-                    "title": u"{}".format(slide.title).encode("utf-8"),
-                    "image": u"{}".format(slide.image.url).encode("utf-8")
-                } for slide in slider]
+            } for promotion in promotions],
 
-            }
+            "slider": [{
+                "title": u"{}".format(slide.title).encode("utf-8"),
+                "image": u"{}".format(slide.image.url).encode("utf-8")
+            } for slide in slider]
+
         })
 
     except:
         return JsonResponse({
-            "result": "Something went wrong"
+            "Something went wrong"
         })
