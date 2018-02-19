95b3ad5f96bdad8e4ce3df79059aee990c7c186b
katalyzator <web.coder96@gmail.com>
2018-02-19 14:39:27 +0600

    database changed

--- asiamall/settings.py
+++ asiamall/settings.py
@@ -88,8 +88,10 @@ WSGI_APPLICATION = 'asiamall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'asiamall',
+        'USER': 'asiamall_admin',
+        'PASSWORD': '123',
     }
 }
 
